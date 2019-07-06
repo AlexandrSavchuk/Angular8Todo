981a838025b73bf0a7aea3ef7f43bb4e6b159950
sav <alexandr.v.savchuk@gmail.com>
2019-07-06 09:30:11 +0300

    create list of todo on page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  date?: any;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-youtube';
+  appTitle = 'Angular todo application';
+
+  public todos: Todo[] = [
+    {id: 1, title: 'купить хлеб', completed: false, date: new Date()},
+    {id: 2, title: 'купить масло', completed: true, date: new Date()},
+    {id: 3, title: 'купить пиво', completed: false, date: new Date()}
+  ];
 }
