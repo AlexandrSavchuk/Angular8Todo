270a14fb04e784e884a60a3deee35ef4cc2d8007
sav <alexandr.v.savchuk@gmail.com>
2019-07-06 12:37:26 +0300

    todos loading from json by http get

--- src/app/shared/todos.service.ts
+++ src/app/shared/todos.service.ts
@@ -1,4 +1,7 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 export interface Todo {
   id: number;
@@ -11,14 +14,25 @@ export interface Todo {
 @Injectable({providedIn: 'root'})
 export class TodosService {
   public todos: Todo[] = [
-    {id: 1, title: 'купить хлеб', completed: false, date: new Date()},
-    {id: 2, title: 'купить масло', completed: true, date: new Date()},
-    {id: 3, title: 'купить пиво', completed: false, date: new Date()}
+    // {id: 1, title: 'купить хлеб', completed: false, date: new Date()},
+    // {id: 2, title: 'купить масло', completed: true, date: new Date()},
+    // {id: 3, title: 'купить пиво', completed: false, date: new Date()}
   ];
+
+  constructor(private http: HttpClient) { }
+
+  fetchTodos(): Observable<Todo[]> {
+    return this.http.get<Todo []> ('https://jsonplaceholder.typicode.com/todos?_limit=3')
+      .pipe(tap(todos => this.todos = todos));
+  }
 
   onToggle(id: number) {
     //console.log(id);
     const idx = this.todos.findIndex(t => t.id === id)
     this.todos[idx].completed = !this.todos[idx].completed;
+  }
+
+  removeTodo(id: number) {
+    this.todos = this.todos.filter(t => t.id !== id);
   }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -9,13 +9,20 @@ import {TodosService} from '../shared/todos.service';
 })
 export class TodosComponent implements OnInit {
 
+  private loading: boolean = true;
+
   constructor(private todosService: TodosService) { }
 
   ngOnInit() {
+    this.todosService.fetchTodos().subscribe( () => {this.loading = false});
   }
 
   onChange(id: number) {
     this.todosService.onToggle(id);
+  }
+
+  removeTodo(id: number) {
+    this.todosService.removeTodo(id);
   }
 
 }
